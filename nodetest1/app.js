088bf6a2b2bcbaadc762168f8cfab2551451f0df
Yogesh Srivastava -X (ysrivast - HCL TECHNOLOGIES LIMITED at Cisco) <ysrivast@partnet.cisco.com>
2017-06-16 22:14:43 +0530

    addinng mongodb

--- nodetest1/app.js
+++ nodetest1/app.js
@@ -5,6 +5,11 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
+//adding mongodb connectivity-yoyo
+var mongo = require('mongodb');
+var monk = require('monk');
+var db = monk('localhost:27017/nodetest1');
+
 var index = require('./routes/index');
 var users = require('./routes/users');
 
@@ -22,8 +27,16 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+// Make our db accessible to our router-yoyo
+app.use(function(req,res,next){
+    req.db = db;
+    next();
+});
+
 app.use('/', index);
 app.use('/users', users);
+
+
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
